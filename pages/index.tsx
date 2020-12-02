6b5427319fcb3a980b65aa92d86b21d25e73834f
Sofia <sofialpizard@gmail.com>
2020-12-02 17:23:19 +0000

    Install styled components

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import { StyledBlue, StyledGreen } from '../styles/globals';
+
+export default function Home() {
+  return (
+    <div>
+      <StyledBlue>Hello World!</StyledBlue>
+      <StyledGreen>Hello World!</StyledGreen>
+    </div>
+  )
+}
---
+++ styles/globals.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const StyledBlue = styled.div`
+  background-color: blue;
+  color: white;
+  font-size: 35px;
+`;
+
+export const StyledGreen = styled.div`
+  background-color: green;
+  color: white;
+  font-size: 35px;
+`;
