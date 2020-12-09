fab8e88a8033e4d130bb277316b812b17254017a
Sofia <sofialpizard@gmail.com>
2020-12-09 16:52:49 +0000

    fix: CM-5015 use hooks for intl

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,20 @@
-const Header = () => (
-  <div
-    style={{
-      backgroundColor: 'darkred',
-      color: 'white',
-      width: '100%',
-      padding: '1em',
-    }}
-  >
-    <h1>Header</h1>
-  </div>
-);
+import { useIntl } from 'react-intl';
+
+const Header = ({ title }: any): React.ReactElement => {
+  const { formatMessage } = useIntl();
+  return (
+    <div
+      style={{
+        backgroundColor: 'darkred',
+        color: 'white',
+        width: '100%',
+        padding: '1em',
+      }}
+    >
+      <h1>{title}</h1>
+      <h2>{formatMessage({ id: 'Hello.World.Test' })}</h2>
+    </div>
+  );
+};
 
 export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 import { StyledBlue, StyledGreen } from '../styles/home';
+import Header from '../components/header';
 
 export default function Home(): React.ReactElement {
+  const { formatMessage } = useIntl();
   return (
     <div>
       <StyledBlue>
@@ -10,6 +12,8 @@ export default function Home(): React.ReactElement {
       <StyledGreen>
         <FormattedMessage id="Hello.World.Test" />
       </StyledGreen>
+      <Header title="Header" />
+      {formatMessage({ id: 'Hello.World.Test' })}
     </div>
   );
 }
